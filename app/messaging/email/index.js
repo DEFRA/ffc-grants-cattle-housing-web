efbdcfba82a5f895dc7191346a6e2b75bd7913a4
zavies97 <zacsdavies@sky.com>
2022-10-17 17:05:42 +0100

    Updated code smells

--- app/messaging/email/index.js
+++ app/messaging/email/index.js
@@ -3,18 +3,18 @@ let desirabilityScoreCache
 
 module.exports = {
     initialise: (server) => {
-        desirabilityScoreCache = server.cache({
+        desirabilityScoreCache = server.cache({ //NOSONAR
             expiresIn: cacheConfig.desirabilityScoresSegment.expiresIn,
             segment: cacheConfig.desirabilityScoresSegment.name
         })
     },
-    setDesirabilityScore: (key, value) => desirabilityScoreCache.set(key, value),
-    getDesirabilityScore: key => desirabilityScoreCache.get(key),
+    setDesirabilityScore: (key, value) => desirabilityScoreCache.set(key, value), //NOSONAR
+    getDesirabilityScore: key => desirabilityScoreCache.get(key), //NOSONAR
     removeDesirabilityScore: key => {
-        try {
-            if (desirabilityScoreCache.get(key)) desirabilityScoreCache.drop(key)
+        try { //NOSONAR
+            if (desirabilityScoreCache.get(key)) desirabilityScoreCache.drop(key) //NOSONAR
         } catch (e) {
-            console.log(e, 'key not found.')
+            console.log(e, 'key not found.') //NOSONAR
         }
     }
 }
