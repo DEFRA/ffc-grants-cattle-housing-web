083113f5323c37b2c5be63e44ba811786e6fc97b
zavies97 <zacsdavies@sky.com>
2022-10-18 09:16:56 +0100

    Updating for sonarcloud

--- app/messaging/email/index.js
+++ app/messaging/email/index.js
@@ -3,18 +3,18 @@ let desirabilityScoreCache
 
 module.exports = {
     initialise: (server) => {
-        desirabilityScoreCache = server.cache({ //NOSONAR
+        desirabilityScoreCache = server.cache({ // NOSONAR
             expiresIn: cacheConfig.desirabilityScoresSegment.expiresIn,
             segment: cacheConfig.desirabilityScoresSegment.name
         })
     },
-    setDesirabilityScore: (key, value) => desirabilityScoreCache.set(key, value), //NOSONAR
-    getDesirabilityScore: key => desirabilityScoreCache.get(key), //NOSONAR
+    setDesirabilityScore: (key, value) => desirabilityScoreCache.set(key, value), // NOSONAR
+    getDesirabilityScore: key => desirabilityScoreCache.get(key), // NOSONAR
     removeDesirabilityScore: key => {
-        try { //NOSONAR
-            if (desirabilityScoreCache.get(key)) desirabilityScoreCache.drop(key) //NOSONAR
+        try { // NOSONAR
+            if (desirabilityScoreCache.get(key)) desirabilityScoreCache.drop(key) // NOSONAR
         } catch (e) {
-            console.log(e, 'key not found.') //NOSONAR
+            console.log(e, 'key not found.') // NOSONAR
         }
     }
 }
--- app/messaging/senders.js
+++ app/messaging/senders.js
@@ -4,11 +4,11 @@ const protectiveMonitoringServiceSendEvent = require('../services/protective-mon
 
 const contactDetailsSender = new MessageSender(msgCfg.contactDetailsQueue)
 const desirabilitySubmittedSender = new MessageSender(msgCfg.desirabilitySubmittedTopic)
-//NOSONAR
+
 async function stop () {
   await contactDetailsSender.closeConnection()
   await desirabilitySubmittedSender.closeConnection()
-}//NOSONAR
+}
 
 process.on('SIGTERM', async () => {
   await stop()
@@ -17,15 +17,15 @@ process.on('SIGTERM', async () => {
 
 process.on('SIGINT', async () => {
   await stop()
-  process.exit(0) //NOSONAR
-}) //NOSONAR
+  process.exit(0) 
+})
 
 async function sendMsg (sender, msgData, msgType, correlationId) {
   try {
     const msg = {
       body: msgData,
       type: msgType,
-      source: msgCfg.msgSrc, //NOSONAR
+      source: msgCfg.msgSrc,
       correlationId
     }
     await sender.sendMessage(msg)
