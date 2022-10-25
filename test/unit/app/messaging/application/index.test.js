c2a410bd1cdc817968c9aad8d2fada949a829e50
zavies97 <zacsdavies@sky.com>
2022-10-25 16:00:44 +0100

    Fixed failing tests

--- test/unit/app/messaging/application/index.test.js
+++ test/unit/app/messaging/application/index.test.js
@@ -19,7 +19,7 @@ describe('application messaging tests', () => {
 
     expect(message).toEqual(receiveMessageRes)
     expect(receiveMessage).toHaveBeenCalledTimes(1)
-    expect(receiveMessage).toHaveBeenCalledWith(sessionId, costResponseQueue)
+    expect(receiveMessage).toHaveBeenCalledWith(sessionId, scoreResponseQueue)
     expect(sendMessage).toHaveBeenCalledTimes(1)
     expect(sendMessage).toHaveBeenCalledWith({}, fetchScoreRequestMsgType, scoreRequestQueue, { sessionId })
   })
