e2bde18c311aa18febc538a89dbb5db21fa35340
yunusfirat <yunusfirat44@gmail.com>
2022-10-27 12:07:13 +0100

    test coverage

--- test/integration/narrow/routes/login.test.js
+++ test/integration/narrow/routes/login.test.js
@@ -36,6 +36,7 @@ describe('login page', () => {
         const postResponse = await global.__SERVER__.inject(postOptions)
         expect(postResponse.statusCode).toBe(200)
         expect(postResponse.payload).toBe('ok')
+        expect(() => require('../../../app/routes/login')).toThrow()
     })
 
     it('login with incorrect details goes to fail action', async () => {
@@ -60,6 +61,19 @@ describe('login page', () => {
             headers: { cookie: 'crumb=' + crumbToken }
         }
 
+        const postResponse = await global.__SERVER__.inject(postOptions)
+        expect(postResponse.statusCode).toBe(302)
+        expect(postResponse.headers.location).toBe('start')
+    })
+
+    // handler
+    xit('redirect to /start when entering correct details', async () => {
+        const postOptions = {
+            method: 'POST',
+            url: `${global.__URLPREFIX__}/login`,
+            payload: { username: 'some conscent', password: 'password', crumb: crumbToken },
+            headers: { cookie: 'crumb=' + crumbToken }
+        }
         const postResponse = await global.__SERVER__.inject(postOptions)
         expect(postResponse.statusCode).toBe(302)
         expect(postResponse.headers.location).toBe('start')
