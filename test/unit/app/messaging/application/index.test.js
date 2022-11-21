9d537bb4a5bf93945933d963fa67a55e955ec6ba
zavies97 <zacsdavies@sky.com>
2022-11-21 11:22:33 +0000

    Updated create-desirability-msg

--- test/unit/app/messaging/application/index.test.js
+++ test/unit/app/messaging/application/index.test.js
@@ -15,7 +15,7 @@ describe('application messaging tests', () => {
     const receiveMessageRes = { id: 1 }
     receiveMessage.mockResolvedValue(receiveMessageRes)
 
-    const message = await getUserScore({}, sessionId)
+    const message = await getUserScore({country: 'Yes', testingValue: 'no'}, sessionId)
 
     expect(message).toEqual(receiveMessageRes)
     expect(receiveMessage).toHaveBeenCalledTimes(1)
@@ -27,7 +27,28 @@ describe('application messaging tests', () => {
           band: null,
           score: null,
         },
-        questions: [],
+        questions: [
+          {
+            answers: [
+              {
+                input: [
+                  {
+                    key: 'country-A1',
+                    value: 'Yes'
+                  }
+                ],
+                key: 'country',
+                title: 'Is the planned project in England?'
+              }
+            ],
+            key: 'country',
+            rating: {
+              band: null,
+              importance: null,
+              score: null
+            }
+          }
+        ],
       },
       grantScheme: {
       key: 'ADDVAL01',
