ef94f96c21feda20017a2afa2b5d89b9657bb84f
zavies97 <zacsdavies@sky.com>
2022-11-21 11:05:25 +0000

    Updated desirability questions json

--- app/messaging/scoring/content-mapping/index.js
+++ app/messaging/scoring/content-mapping/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   desirabilityQuestions: require('./desirability-questions'),
   desirabilityInputQuestionMapping: {
+    productsProcessed: 'products-processed',
     country: 'country'
   }
 }
