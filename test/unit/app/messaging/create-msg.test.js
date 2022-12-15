8d4cb404e99b18a37d74b31ce53ea64594d058ea
yunusfirat <yunusfirat44@gmail.com>
2022-12-15 12:38:06 +0000

    Revert "commented create-msg"
    This reverts commit 29b7cd2235997e4514f0d0b9434e2cb7db526a8d.

--- test/unit/app/messaging/create-msg.test.js
+++ test/unit/app/messaging/create-msg.test.js
@@ -1,47 +1,47 @@
-// describe('create-msg', () => {
-//   jest.mock('../../../../app/helpers/session')
-//   const { getYarValue } = require('../../../../app/helpers/session')
+describe('create-msg', () => {
+  jest.mock('../../../../app/helpers/session')
+  const { getYarValue } = require('../../../../app/helpers/session')
 
-//   const { getDesirabilityAnswers } = require('../../../../app/messaging/create-msg')
+  const { getDesirabilityAnswers } = require('../../../../app/messaging/create-msg')
 
-//   test('check getDesirabilityAnswers()', () => {
-//     let dict
-//     getYarValue.mockImplementation((req, key) => (dict[key]))
+  test('check getDesirabilityAnswers()', () => {
+    let dict
+    getYarValue.mockImplementation((req, key) => (dict[key]))
 
-//     dict = {
-//       productsProcessed: 'prod-processed',
-//       howAddingValue: 'how-av',
-//       projectImpact: ['proj-imp'],
-//       futureCustomers: ['future-cust'],
-//       collaboration: 'collaboration',
-//       environmentalImpact: 'env-imp'
-//     }
-//     expect(getDesirabilityAnswers({})).toEqual({
-//       productsProcessed: 'prod-processed',
-//       howAddingValue: 'how-av',
-//       projectImpact: ['proj-imp'],
-//       futureCustomers: ['future-cust'],
-//       collaboration: 'collaboration',
-//       environmentalImpact: ['env-imp']
-//     })
+    dict = {
+      productsProcessed: 'prod-processed',
+      howAddingValue: 'how-av',
+      projectImpact: ['proj-imp'],
+      futureCustomers: ['future-cust'],
+      collaboration: 'collaboration',
+      environmentalImpact: 'env-imp'
+    }
+    expect(getDesirabilityAnswers({})).toEqual({
+      productsProcessed: 'prod-processed',
+      howAddingValue: 'how-av',
+      projectImpact: ['proj-imp'],
+      futureCustomers: ['future-cust'],
+      collaboration: 'collaboration',
+      environmentalImpact: ['env-imp']
+    })
 
-//     dict = {
-//       ...dict,
-//       environmentalImpact: ['env-imp']
-//     }
-//     expect(getDesirabilityAnswers({})).toEqual({
-//       productsProcessed: 'prod-processed',
-//       howAddingValue: 'how-av',
-//       projectImpact: ['proj-imp'],
-//       futureCustomers: ['future-cust'],
-//       collaboration: 'collaboration',
-//       environmentalImpact: ['env-imp']
-//     })
+    dict = {
+      ...dict,
+      environmentalImpact: ['env-imp']
+    }
+    expect(getDesirabilityAnswers({})).toEqual({
+      productsProcessed: 'prod-processed',
+      howAddingValue: 'how-av',
+      projectImpact: ['proj-imp'],
+      futureCustomers: ['future-cust'],
+      collaboration: 'collaboration',
+      environmentalImpact: ['env-imp']
+    })
 
-//     dict = {
-//       ...dict,
-//       howAddingValue: ['how-av']
-//     }
-//     expect(getDesirabilityAnswers({})).toEqual(null)
-//   })
-// })
+    dict = {
+      ...dict,
+      howAddingValue: ['how-av']
+    }
+    expect(getDesirabilityAnswers({})).toEqual(null)
+  })
+})
