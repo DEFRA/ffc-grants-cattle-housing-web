aa63fb69d893550f4a70029b53fd0028623a6e77
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-03-09 03:46:21 +0000

    Fix validations

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -1730,11 +1730,15 @@ const questionBank = {
                 text: 'Maximum number of calves housed annually after project'
               },
               validate: [
-                
+                {
+                  type: 'REGEX',
+                  regex: ONLY_DIGITS_REGEX,
+                  error: 'Number of calves should only include numbers'
+                },
                 {
                   type: 'REGEX',
                   regex: WHOLE_NUMBER_REGEX,
-                  error: [ 'Number of calves must be a whole number', 'Number of calves should only include numbers' ]
+                  error: 'Number of calves must be a whole number', 
                 },
                 {
                   type: 'MIN_MAX',
