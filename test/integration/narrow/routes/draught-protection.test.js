72ab86d6f7370530a70499cc60e46bfb6e6b6a84
diya2214 <madihakhn@yahoo.com>
2023-03-16 08:45:18 +0000

    comment and indent fix

--- test/integration/narrow/routes/draught-protection.test.js
+++ test/integration/narrow/routes/draught-protection.test.js
@@ -1,25 +1,25 @@
 const { crumbToken } = require('./test-helper')
 
 describe('Page: /draught-protection', () => {
-const varList = {
+  const varList = {
     legalStatus: 'randomData',
     projectType: 'fakeData',
     tenancy : 'Yes',
     tenancyLength: null
-}
+  }
 
-jest.mock('../../../../app/helpers/session', () => ({
+  jest.mock('../../../../app/helpers/session', () => ({
     setYarValue: (request, key, value) => null,
     getYarValue: (request, key) => {
-    if (varList[key]) return varList[key]
-    else return 'Error'
+      if (varList[key]) return varList[key]
+      else return 'Error'
     }
-}))
+  }))
 
-it('page loads successfully, with all the options', async () => {
+  it('page loads successfully, with all the options', async () => {
     const options = {
-    method: 'GET',
-    url: `${global.__URLPREFIX__}/draught-protection`
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/draught-protection`
     }
 
     const response = await global.__SERVER__.inject(options)
@@ -27,53 +27,53 @@ it('page loads successfully, with all the options', async () => {
     expect(response.payload).toContain('Will calves be protected from draughts?')
     expect(response.payload).toContain('Yes')
     expect(response.payload).toContain('No')
-})
+  })
 
-it('no option selected -> show error message', async () => {
+  it('no option selected -> show error message', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/draught-protection`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: { crumb: crumbToken }
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/draught-protection`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { crumb: crumbToken }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(200)
     expect(postResponse.payload).toContain('Select yes if the calves will be protected from draughts')
-})
+  })
 
-it('user selects ineligible option: \'No \' -> display ineligible page', async () => {
+  it('user selects ineligible option: \'No \' -> display ineligible page', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/draught-protection`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: { draughtProtection: 'No', crumb: crumbToken }
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/draught-protection`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { draughtProtection: 'No', crumb: crumbToken }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
-})
+  })
 
-it('user selects eligible option -> store user response and redirect to /additional-items', async () => {
+  it('user selects eligible option -> store user response and redirect to /additional-items', async () => {
     const postOptions = {
-    method: 'POST',
-    url: `${global.__URLPREFIX__}/draught-protection`,
-    headers: { cookie: 'crumb=' + crumbToken },
-    payload: { draughtProtection: 'Yes', crumb: crumbToken }
+      method: 'POST',
+      url: `${global.__URLPREFIX__}/draught-protection`,
+      headers: { cookie: 'crumb=' + crumbToken },
+      payload: { draughtProtection: 'Yes', crumb: crumbToken }
     }
 
     const postResponse = await global.__SERVER__.inject(postOptions)
     expect(postResponse.statusCode).toBe(302)
     expect(postResponse.headers.location).toBe('additional-items')
-})
+  })
 
-it('page loads with correct back link', async () => {
+  it('page loads with correct back link', async () => {
     const options = {
-    method: 'GET',
-    url: `${global.__URLPREFIX__}/draught-protection`
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/draught-protection`
     }
     const response = await global.__SERVER__.inject(options)
     expect(response.statusCode).toBe(200)
     expect(response.payload).toContain('<a href=\"drainage-slope\" class=\"govuk-back-link\">Back</a>')
-})
+  })
 })
--- test/integration/narrow/routes/pageGuard.test.js
+++ test/integration/narrow/routes/pageGuard.test.js
@@ -1,43 +1,43 @@
-const createServer = require('../../../../app/server')
-const { startPageUrl } = require('../../../../app/config/server')
+// const createServer = require('../../../../app/server')
+// const { startPageUrl } = require('../../../../app/config/server')
 
-describe('Page Guard', () => {
-  const OLD_ENV = process.env
-  let server
+// describe('Page Guard', () => {
+//   const OLD_ENV = process.env
+//   let server
 
-  beforeEach(async () => {
-    jest.resetModules()
-    process.env = { ...OLD_ENV }
-  })
+//   beforeEach(async () => {
+//     jest.resetModules()
+//     process.env = { ...OLD_ENV }
+//   })
 
-  afterEach(() => {
-    process.env = OLD_ENV
-    server.stop()
-  })
+//   afterEach(() => {
+//     process.env = OLD_ENV
+//     server.stop()
+//   })
 
-  // it('shoud redirect to start page if the site is decommissioned', async () => {
-  //   process.env.SERVICE_END_DATE = '2021/02/17'
-  //   process.env.SERVICE_END_TIME = '23:59:58'
-  //   server = await createServer()
-  //   const getOptions = {
-  //     method: 'GET',
-  //     url: `${global.__URLPREFIX__}/legal-status`
-  //   }
+//   xit('shoud redirect to start page if the site is decommissioned', async () => {
+//     process.env.SERVICE_END_DATE = '2021/02/17'
+//     process.env.SERVICE_END_TIME = '23:59:58'
+//     server = await createServer()
+//     const getOptions = {
+//       method: 'GET',
+//       url: `${global.__URLPREFIX__}/legal-status`
+//     }
 
-  //   const getResponse = await server.inject(getOptions)
-  //   expect(getResponse.statusCode).toBe(302)
-  //   expect(getResponse.headers.location).toBe(startPageUrl)
-  // })
+//     const getResponse = await server.inject(getOptions)
+//     expect(getResponse.statusCode).toBe(302)
+//     expect(getResponse.headers.location).toBe(startPageUrl)
+//   })
 
-  // it('should redirect to start page if the user skip journey question', async () => {
-  //   server = await createServer()
-  //   const getOptions = {
-  //     method: 'GET',
-  //     url: `${global.__URLPREFIX__}/standard-costs`
-  //   }
+//   it('should redirect to start page if the user skip journey question', async () => {
+//     server = await createServer()
+//     const getOptions = {
+//       method: 'GET',
+//       url: `${global.__URLPREFIX__}/project-cost`
+//     }
 
-  //   const response = await server.inject(getOptions)
-  //   expect(response.statusCode).toBe(302)
-  //   expect(response.headers.location).toBe(startPageUrl)
-  // })
-})
+//     const response = await server.inject(getOptions)
+//     expect(response.statusCode).toBe(302)
+//     expect(response.headers.location).toBe(startPageUrl)
+//   })
+// })
