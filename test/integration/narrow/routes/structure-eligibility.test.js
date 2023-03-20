8a8c9b15780def6a24daf7c8476f005114e02fe6
Yunus FIRAT <yunusfirat44@gmail.com>
2023-03-20 17:12:50 +0000

    structure-eligibility test

---
+++ test/integration/narrow/routes/structure-eligibility.test.js
@@ -0,0 +1,65 @@
+const { crumbToken } = require('./test-helper')
+
+describe('Page: /structure-eligibility', () => {
+const varList = {
+    legalStatus: 'randomData',
+    projectType: 'fakeData'
+}
+
+jest.mock('../../../../app/helpers/session', () => ({
+    setYarValue: (request, key, value) => null,
+    getYarValue: (request, key) => {
+    if (varList[key]) return varList[key]
+    else return 'Error'
+    }
+}))
+
+it('page loads successfully, with all the options', async () => {
+    const options = {
+    method: 'GET',
+    url: `${global.__URLPREFIX__}/structure-eligibility`
+    }
+
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('Does your building structure meet the eligibility criteria?')
+    expect(response.payload).toContain('Yes')
+    expect(response.payload).toContain('No')
+})
+
+it('no option selected -> show error message', async () => {
+    const postOptions = {
+    method: 'POST',
+    url: `${global.__URLPREFIX__}/structure-eligibility`,
+    headers: { cookie: 'crumb=' + crumbToken },
+    payload: { crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('Select yes if your building structure meets the eligibility criteria')
+})
+
+it('user selects eligible option -> store user response and redirect to /drainage-slope', async () => {
+    const postOptions = {
+    method: 'POST',
+    url: `${global.__URLPREFIX__}/structure-eligibility`,
+    headers: { cookie: 'crumb=' + crumbToken },
+    payload: { structureEligibility: 'Yes', crumb: crumbToken }
+    }
+
+    const postResponse = await global.__SERVER__.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('drainage-slope')
+})
+
+it('page loads with correct back link', async () => {
+    const options = {
+    method: 'GET',
+    url: `${global.__URLPREFIX__}/structure-eligibility`
+    }
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toContain('<a href=\"structure\" class=\"govuk-back-link\">Back</a>')
+})
+})
