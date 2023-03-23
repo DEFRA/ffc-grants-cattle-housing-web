22be71413a86a5f28a8bc552ae09f2703262fa9f
Yunus FIRAT <yunusfirat44@gmail.com>
2023-03-23 16:59:00 +0000

    update number of employees.

--- app/config/question-bank.js
+++ app/config/question-bank.js
@@ -2548,7 +2548,7 @@ const questionBank = {
             {
               yarKey: 'numberEmployees',
               type: 'number',
-              classes: 'govuk-input--width-10',
+              classes: 'govuk-input--width-4',
               label: {
                 text: 'Number of employees',
                 classes: 'govuk-label'
@@ -2565,12 +2565,6 @@ const questionBank = {
                   type: 'REGEX',
                   regex: WHOLE_NUMBER_REGEX,
                   error: 'Number of employees must be a whole number, like 305'
-                },
-                {
-                  type: 'MIN_MAX_CHARS',
-                  min: 1,
-                  max: 4,
-                  error: 'Number must be between 1-9999'
                 },
                 {
                   type: 'MIN_MAX',
