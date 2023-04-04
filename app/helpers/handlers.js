a6383e5b2ebf02b08fc2c4454cd8a5bcc7d786bd
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-04-04 08:54:04 +0100

    Fix living space redirection

--- app/helpers/handlers.js
+++ app/helpers/handlers.js
@@ -158,18 +158,22 @@ const getPage = async (question, request, h) => {
       if(getYarValue(request,'tenancy') === 'Yes'){
         setYarValue(request, 'tenancyLength', null)
       }
+      break
     }
     case 'living-space-3m2': {
       setYarValue(request, 'livingSpace4m2', null)
       setYarValue(request, 'livingSpace5m2', null)
+      break
     }
     case 'living-space-4m2': {
       setYarValue(request, 'livingSpace3m2', null)
       setYarValue(request, 'livingSpace5m2', null)
+      break
     }
     case 'living-space-5m2': {
       setYarValue(request, 'livingSpace3m2', null)
       setYarValue(request, 'livingSpace4m2', null)
+      break
     }
     case 'score': {
 
