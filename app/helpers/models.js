c5c64f3d98551da04bd96666bb39d4a9c68caaf6
zavies97 <zavies97@users.noreply.github.com>
2023-04-06 09:32:31 +0100

    remove unnecessary code in models.js

--- app/helpers/models.js
+++ app/helpers/models.js
@@ -3,15 +3,15 @@ const { getOptions } = require('../helpers/answer-options')
 const { getYarValue, setYarValue } = require('../helpers/session')
 const { getQuestionByKey, allAnswersSelected } = require('../helpers/utils')
 
-const getPrefixSufixString = (prefixSufix, selectedValueOfLinkedQuestion) => {
-  if (prefixSufix.linkedPrefix || prefixSufix.linkedSufix) {
-    selectedValueOfLinkedQuestion = prefixSufix.linkedPrefix.concat(selectedValueOfLinkedQuestion)
-  }
-  if (prefixSufix.linkedSufix) {
-    selectedValueOfLinkedQuestion = selectedValueOfLinkedQuestion.concat(prefixSufix.linkedSufix)
-  }
-  return selectedValueOfLinkedQuestion
-}
+// const getPrefixSufixString = (prefixSufix, selectedValueOfLinkedQuestion) => {
+//   if (prefixSufix.linkedPrefix || prefixSufix.linkedSufix) {
+//     selectedValueOfLinkedQuestion = prefixSufix.linkedPrefix.concat(selectedValueOfLinkedQuestion)
+//   }
+//   if (prefixSufix.linkedSufix) {
+//     selectedValueOfLinkedQuestion = selectedValueOfLinkedQuestion.concat(prefixSufix.linkedSufix)
+//   }
+//   return selectedValueOfLinkedQuestion
+// }
 
 const getDependentSideBar = (sidebar, request) => {
   const { values, dependentQuestionKeys } = sidebar
@@ -29,15 +29,15 @@ const getDependentSideBar = (sidebar, request) => {
       const yarValueOfLinkedQuestion = getQuestionByKey(sidebar.linkedQuestionkey[index]).yarKey
       let selectedValueOfLinkedQuestion = getYarValue(request, yarValueOfLinkedQuestion)
 
-      if (selectedValueOfLinkedQuestion && sidebar.prefixSufix) {
-        selectedValueOfLinkedQuestion = getPrefixSufixString(sidebar.prefixSufix[index], selectedValueOfLinkedQuestion)
-      }
+      // if (selectedValueOfLinkedQuestion && sidebar.prefixSufix) {
+      //   selectedValueOfLinkedQuestion = getPrefixSufixString(sidebar.prefixSufix[index], selectedValueOfLinkedQuestion)
+      // }
 
-      if (selectedValueOfLinkedQuestion && values[index].content[0].items[0] !== 'Not needed') {
-        values[index].content[0].items.push(selectedValueOfLinkedQuestion)
-      } else {
-        setYarValue(request, 'coverSize', '')
-      }
+      // if (selectedValueOfLinkedQuestion && values[index].content[0].items[0] !== 'Not needed') {
+      //   values[index].content[0].items.push(selectedValueOfLinkedQuestion)
+      // } else {
+      //   setYarValue(request, 'coverSize', '')
+      // }
     }
   })
   return {
