44cf7cf8f2cab874ccaa9537d31227b9752c7bc8
Sal Abuewilly <sal.abuewilly@gmail.com>
2023-04-06 11:18:44 +0100

    Fix back to score

--- app/helpers/models.js
+++ app/helpers/models.js
@@ -78,9 +78,43 @@ const showBackToEvidenceSummaryButton = (key, request) => {
 
 const showBackToEScoreSummaryButton = (key, request) => {
   switch (key) {
-    // case 'planning-permission':
-    // case 'planning-permission-evidence':
-    // case 'moisture-control':
+    // case '':
+    case 'applicant-type':
+    case 'legal-status':
+    case 'country':
+    case 'planning-permission':
+    case 'project-started':
+    case 'tenancy':
+    case 'project':
+    case 'calf-weight':
+    case 'living-space-3m2':
+    case 'living-space-4m2':
+    case 'living-space-5m2':
+    case 'housed-individually':
+    case 'isolate-calves':
+    case 'straw-bedding':
+    case 'concrete-flooring':
+    case 'enrichment':
+    case 'structure':
+    case 'structure-eligibility':
+    case 'drainage-slope':
+    case 'draught-protection':
+    case 'additional-items':
+    case 'lighting':
+    case 'roof-solar-PV':
+    case 'project-cost':
+    case 'housing':
+    case 'group-size':
+    case 'number-of-calves':
+    case 'automatic-calf-feeder':
+    case 'moisture-control':
+    case 'permanent-sick-pen':
+    case 'floor-space-100kg-150kg':
+    case 'floor-space-under100kg':
+    case 'floor-space-over150kg':
+    case 'environmental-impact':
+    case 'sustainable-materials':
+    case 'introducing-innovation':
     case 'score': {
       return !!getYarValue(request, 'overAllScore')
     }
