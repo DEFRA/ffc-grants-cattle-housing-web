7ebca88ff8ad8861b454bbc6ffea6ffd30920acf
diya2214 <madihakhn@yahoo.com>
2023-04-17 11:09:21 +0100

    update ref number

--- app/helpers/pageHelpers.js
+++ app/helpers/pageHelpers.js
@@ -3,7 +3,7 @@ const { setOptionsLabel } = require('../helpers/answer-options')
 const { getYarValue, setYarValue } = require('../helpers/session')
 
 const getConfirmationId = (guid) => {
-  const prefix = 'UCH'
+  const prefix = 'CH'
   return `${prefix}-${guid.substr(0, 3)}-${guid.substr(3, 3)}`.toUpperCase()
 }
 
