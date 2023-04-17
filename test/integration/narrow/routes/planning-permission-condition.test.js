8a9d07ff695fa42531985656d2b8b665fec93e1f
diya2214 <madihakhn@yahoo.com>
2023-04-17 17:20:11 +0100

    planning permission condition fix

--- test/integration/narrow/routes/planning-permission-condition.test.js
+++ test/integration/narrow/routes/planning-permission-condition.test.js
@@ -24,17 +24,12 @@ describe('Page: /planning-permission-condition', () => {
     expect(getResponse.payload).toContain('You may be able to apply for a grant from this scheme')
   })
   it('page loads with correct back link', async () => {
-    const postOptions = {
-      method: 'POST',
-      url: `${global.__URLPREFIX__}/planning-permission`,
-      headers: { cookie: 'crumb=' + crumbToken },
-      payload: { planningPermission: 'Should be in place by 31 January 2024', crumb: crumbToken }
+    const getOptions = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/planning-permission-condition`
     }
-
-    const postResponse = await global.__SERVER__.inject(postOptions)
-    expect(postResponse.statusCode).toBe(302)
-    expect(postResponse.headers.location).toBe('planning-permission-condition')
-    expect(postResponse.payload).toContain('<a href=\"planning-permission`\" class=\"govuk-back-link\">Back</a>')
-
+    const getResponse = await global.__SERVER__.inject(getOptions)
+    expect(getResponse.headers.location).toBe('planning-permission-condition')
+    expect(getResponse.payload).toContain('<a href=\"planning-permission`\" class=\"govuk-back-link\">Back</a>')
   })
 })
