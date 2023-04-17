ffec0832e0e4dd744826be68283cefb60d5d87a8
zavies97 <zavies97@users.noreply.github.com>
2023-04-17 15:03:07 +0100

    updated score mock

--- test/data/score-data.js
+++ test/data/score-data.js
@@ -1,243 +1,208 @@
 
 const msgData = {
-  grantScheme: {
-    key: 'Calf01',
-    name: 'Cattle Housing Grant'
+  "grantScheme": {
+      "key": "CALF01",
+      "name": "Upgrading Calf Housing"
   },
-  desirability: {
-    questions: [
-      {
-        key: "housing",
-        answers: [
+  "desirability": {
+      "questions": [
           {
-            key: 'housing',
-            title: 'Housing',
-            input: [
-              {
-                key: 'housing-A1',
-                value: 'Yes'
+              "key": "housing",
+              "answers": [
+                  {
+                      "key": "housing",
+                      "title": "Are you moving from individually housing calves over 7 days old to pair or group housing?",
+                      "input": [
+                          {
+                              "key": "housing-A1",
+                              "value": "Yes"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {
+                  "score": 14.76,
+                  "band": "Strong",
+                  "importance": null
               }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "calf-group-size",
-        answers: [
+          },
           {
-            key: 'calf-group-size',
-            title: 'Calf group size',
-            input: [
-              {
-                key: 'calf-group-size-A1',
-                value: '2 to 3'
+              "key": "calf-group-size",
+              "answers": [
+                  {
+                      "key": "calf-group-size",
+                      "title": "What will be the average group size for calves over 7 days old?",
+                      "input": [
+                          {
+                              "key": "calf-group-size-A1",
+                              "value": "2 to 3"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {
+                  "score": 2.9469,
+                  "band": "Strong",
+                  "importance": null
               }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "number-of-calves",
-        answers: [
+          },
           {
-            key: 'number-of-calves',
-            title: 'CNumber of calves',
-            input: [
-              {
-                key: 'number-of-calves-A1',
-                value: '2 to 50'
+              "key": "number-of-calves",
+              "answers": [
+                  {
+                      "key": "number-of-calves",
+                      "title": "What will be the maximum number of calves in the calf housing?",
+                      "input": [
+                          {
+                              "key": "number-of-calves-A2",
+                              "value": "51 to 100"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {
+                  "score": 0,
+                  "band": "Weak",
+                  "importance": null
               }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "automatic-calf-feeder",
-        answers: [
+          },
           {
-            key: 'automatic-calf-feeder',
-            title: 'Automatic Calf Feeder',
-            input: [
-              {
-                key: 'automatic-calf-feeder-A1',
-                value: '1 to 4'
+              "key": "automatic-calf-feeder",
+              "answers": [
+                  {
+                      "key": "automatic-calf-feeder",
+                      "title": "How many calves will you have per automatic feeder?",
+                      "input": [
+                          {
+                              "key": "automatic-calf-feeder-A3",
+                              "value": "9 to 12"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {
+                  "score": 0.7293000000000001,
+                  "band": "Strong",
+                  "importance": null
               }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "calf-group-size",
-        answers: [
+          },
           {
-            key: 'calf-group-size',
-            title: 'Calf group size',
-            input: [
-              {
-                key: 'calf-group-size-A1',
-                value: '2 to 3'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "moisture-control",
-        answers: [
+              "key": "moisture-control",
+              "answers": [
+                  {
+                      "key": "moisture-control",
+                      "title": "How will your building control moisture?",
+                      "input": [
+                          {
+                              "key": "moisture-control-A2",
+                              "value": "Positioning drinking areas near drainage and away from bedding"
+                          },
+                          {
+                              "key": "moisture-control-A3",
+                              "value": "A separate preparation or washing area"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {}
+          },
           {
-            key: 'moisture-control',
-            title: 'Moisture control',
-            input: [
-              {
-                key: 'moisture-control-A1',
-                value: 'A drain or drainage channel inside the pen'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "permanent-sick-pen",
-        answers: [
+              "key": "permanent-sick-pen",
+              "answers": [
+                  {
+                      "key": "permanent-sick-pen",
+                      "title": "What type of sick pen will your building have?",
+                      "input": [
+                          {
+                              "key": "permanent-sick-pen-A1",
+                              "value": "A permanent sick pen"
+                          },
+                          {
+                              "key": "permanent-sick-pen-A2",
+                              "value": "A separate air space"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {}
+          },
           {
-            key: 'permanent-sick-pen',
-            title: 'Permanent sick pen',
-            input: [
-              {
-                key: 'permanent-sick-pen-A1',
-                value: 'A permanent sick pen'
+              "key": "floor-space",
+              "answers": [
+                  {
+                      "key": "floor-space",
+                      "title": "How much space will each calf have?",
+                      "input": [
+                          {
+                              "key": 3,
+                              "value": 5454
+                          }
+                      ]
+                  }
+              ],
+              "rating": {
+                  "score": 2725500,
+                  "band": "Strong",
+                  "importance": null
               }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "floor-space",
-        answers: [
+          },
           {
-            key: 'floor-space',
-            title: 'Floor space',
-            input: [
-              {
-                key: null,
-                value: 20
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: "floor-space",
-        answers: [
+              "key": "environmental-impact",
+              "answers": [
+                  {
+                      "key": "environmental-impact",
+                      "title": "How will the building minimise environmental impact?",
+                      "input": [
+                          {
+                              "key": "environmental-impact-A2",
+                              "value": "Collect and store rainwater"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {}
+          },
           {
-            key: 'floor-space',
-            title: 'Floor space',
-            input: [
-              {
-                key: null,
-                value: 20
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Average'
-        }
-        
-      },
-      {
-        key: 'environmental-impact',
-        answers: [
+              "key": "sustainable-materials",
+              "answers": [
+                  {
+                      "key": "sustainable-materials",
+                      "title": "Will your building use sustainable materials?",
+                      "input": [
+                          {
+                              "key": "sustainable-materials-A1",
+                              "value": "Low carbon concrete"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {}
+          },
           {
-            key: 'environmental-impact',
-            title: 'How will the project improve the environment?',
-            input: [
-              {
-                key: 'environmental-impact-A1',
-                value: 'Solar PV panels on the roof of the building'
-              },
-              {
-                key: 'environmental-impact-A2',
-                value: 'Collect and store rainwater'
-              }
-            ]
+              "key": "introducing-innovation",
+              "answers": [
+                  {
+                      "key": "introducing-innovation",
+                      "title": "Is your project introducing innovation?",
+                      "input": [
+                          {
+                              "key": "introducing-innovation-A1",
+                              "value": "Technology"
+                          },
+                          {
+                              "key": "introducing-innovation-A3",
+                              "value": "Techniques"
+                          }
+                      ]
+                  }
+              ],
+              "rating": {}
           }
-        ],
-        rating: {
-          score: 0.8,
-          band: 'Strong'
-        }
-      },
-      {
-        key: 'sustainable-materials',
-        answers: [
-          {
-            key: 'sustainable-materials',
-            title: 'Sustainable materials',
-            input: [
-              {
-                key: 'sustainable-materials-A1',
-                value: 'Low carbon concrete'
-              },
-              {
-                key: 'sustainable-materials-A2',
-                value: 'Steel replacement products'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 0.8,
-          band: 'Strong'
-        }
+      ],
+      "overallRating": {
+          "score": null,
+          "band": "Average"
       }
-    ],
-      
-    overallRating: {
-      score: 19,
-      band: 'Average'
-    }
   }
 }
 module.exports = msgData
