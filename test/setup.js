9356bce644cadc0158255d834c09ae05905d75d2
zavies97 <zavies97@users.noreply.github.com>
2023-05-04 11:22:27 +0100

    update test setup mocking (#111)
    * update test setup mocking
    
    * Update package.json

--- test/setup.js
+++ test/setup.js
@@ -11,6 +11,23 @@ beforeEach(async () => {
   }
 
   jest.mock('../app/cookies/index', () => mockSession)
+  jest.mock('../app/services/gapi-service.js')
+  jest.mock('../app/services/app-insights.js')
+  jest.mock('../app/services/protective-monitoring-service.js')
+  jest.mock('../app/config/auth', () => {
+    return {
+      credentials: {
+        username: '',
+        passwordHash: ''
+      },
+      cookie: {
+        name: 'session-auth',
+        password: '',
+        isSecure: false
+      },
+      enabled: false
+    }
+  })
 
   const server = await createServer()
   await server.start()
