6a476692bd721bc9e374c8671b75f07e4e69dbce
diya2214 <madihakhn@yahoo.com>
2023-06-22 12:00:04 +0100

    clean up code

--- app/services/gapi-service.js
+++ app/services/gapi-service.js
@@ -1,20 +1,13 @@
 const appInsights = require('./app-insights')
 const { getYarValue } = require('../helpers/session')
 const blockDefaultPageViews = ['start', 'applying']
-// const blockDefaultPageViews = [
-//   'start', 'applying', 'confirmation', 'products-processed', 'remaining-costs',
-//   'project-cost', 'project-start', 'planning-permission', 'score',
-//   'country', 'legal-status', 'farming-type'
-// ]
+
 const isBlockDefaultPageView = (url) => {
   const currentUrl = url.pathname.split('/').pop().toString().toLowerCase()
   return blockDefaultPageViews.indexOf(currentUrl) >= 0
 }
 
-
-// const grant_type = 'Slurry Infrastructure'
-// or is it...?
-const grant_type = 'Animal Health and Wellfare'
+const grant_type = 'Upgrading Calf Housing'
 
 const eventTypes = {
   PAGEVIEW: 'pageview',
@@ -27,15 +20,12 @@ const eventTypes = {
 
 const sendGAEvent = async (request, metrics) => {
   const timeSinceStart = getTimeofJourneySinceStart(request).toString()
-  const page_path = request.route.path
+  const pagePath = request.route.path
   const { name, params } = metrics
   const isEliminationEvent = name === eventTypes.ELIMINATION
   const isEligibilityEvent = name === eventTypes.ELIGIBILITY
   const isScoreEvent = name === eventTypes.SCORE
   const isConfirmationEvent = name === eventTypes.CONFIRMATION
-
-  const url = JSON.stringify(request.headers);
-
   const dmetrics = {
     ...params,
     ...(isEliminationEvent && { elimination_time: timeSinceStart }),
@@ -44,9 +34,10 @@ const sendGAEvent = async (request, metrics) => {
     ...(isConfirmationEvent && { final_score: getYarValue(request, 'current-score'), user_type: getYarValue(request, 'applying'), confirmation_time: timeSinceStart }),
     ...(params?.score_presented && { score_presented: params.score_presented }),
     grant_type,
-    page_path,
+    page_title: pagePath
   }
   try {
+    console.log('dmetrics:::::: ', dmetrics)
     const event = { name, params: dmetrics }
     await request.ga.view(request, [event])
     console.log('Metrics Sending analytics %s for %s', name, request.route.path)
