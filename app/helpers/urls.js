0300dedc5ded842872aa1e139e067bc219b9a0b5
diya2214 <madihakhn@yahoo.com>
2023-08-24 17:15:39 +0100

    new feature to url object

--- app/helpers/urls.js
+++ app/helpers/urls.js
@@ -14,27 +14,35 @@ const getUrl = (urlObject, url, request, secBtn, currentUrl) => {
   const { dependentQuestionYarKey, dependentAnswerKeysArray, urlOptions, nonDependentAnswerKeysArray = [] } = urlObject
   const { thenUrl, elseUrl, nonDependentUrl } = urlOptions
 
-  const dependentAnswer = getYarValue(request, dependentQuestionYarKey)
-
-  const selectThenUrl = checkAnswerExist(dependentQuestionYarKey, dependentAnswer, dependentAnswerKeysArray)
-  const isNonDependantAnswer = checkAnswerExist(dependentQuestionYarKey, dependentAnswer, nonDependentAnswerKeysArray)
-  const selectedElseUrl = (dependentAnswer && !isNonDependantAnswer) ? elseUrl : nonDependentUrl
+  const selectThenUrl = checkAnswerExist([dependentQuestionYarKey].flat(), request, dependentAnswerKeysArray)
+  const isNonDependantAnswer = checkAnswerExist([dependentQuestionYarKey].flat(), request, nonDependentAnswerKeysArray)
+  const selectedElseUrl = checkSelectElseUrl(request, [dependentQuestionYarKey].flat(), isNonDependantAnswer) ? elseUrl : nonDependentUrl
 
   const nextUrl = selectThenUrl ? thenUrl : selectedElseUrl
 
   return secBtn ? secBtnPath : nextUrl
 }
-const checkAnswerExist = (dependentQuestionYarKey, dependentAnswer, yarKeysToCheck) => {
-  return ALL_QUESTIONS.find(thisQuestion => (
-    thisQuestion.yarKey === dependentQuestionYarKey &&
-    thisQuestion.answers &&
-    thisQuestion.answers.some(answer => (
-      !!dependentAnswer &&
-      yarKeysToCheck.includes(answer.key) &&
-      dependentAnswer.includes(answer.value)
+const checkAnswerExist = (dependentQuestionYarKey, request, yarKeysToCheck) => {
+  return dependentQuestionYarKey.some(questionYarKey => {
+    const dependentAnswer = getYarValue(request, questionYarKey)
+    return !!ALL_QUESTIONS.find(thisQuestion => (
+      thisQuestion.yarKey === questionYarKey &&
+      thisQuestion.answers &&
+      thisQuestion.answers.some(answer => (
+        !!dependentAnswer &&
+        yarKeysToCheck.includes(answer.key) &&
+        dependentAnswer.includes(answer.value)
+      ))
     ))
-  ))
+  })
 }
+
+const checkSelectElseUrl = (request, dependentQuestionYarKey, isNonDependantAnswer) => {
+  return dependentQuestionYarKey.some(dependentQuestionYarKey => {
+    return getYarValue(request, dependentQuestionYarKey) && !isNonDependantAnswer
+  })
+}
+
 module.exports = {
   getUrl
 }
